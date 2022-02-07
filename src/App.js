6bf07964a5959301b93947908448695e88b9b494
Bekzod13 <bekzodjonsharobiddinov@gmail.com>
2022-02-07 09:38:28 +0500

    carusel updated

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import { useState } from 'react';
+import Slider from 'react-slick';
+import './App.css';
+
+// import icons
+import {BsArrowLeft, BsArrowRight, BsBellSlash, BsBellFill} from 'react-icons/bs';
+
+// import images
+import img1 from './images/img1.jpg';
+import img2 from './images/img2.jpg';
+import img3 from './images/img3.jpg';
+import img4 from './images/img4.jpg';
+import logo from './images/logo.jpg';
+
+const images = [img1, img2, img3, img4];
+
+function SampleNextArrow({onClick}) {
+  return (
+    <div className='arrow arrow-right' onClick={onClick}>
+      <BsArrowRight/>
+    </div>
+  );
+}
+
+function SamplePrevArrow({onClick}) {
+  return (
+    <div className='arrow arrow-left' onClick={onClick}>
+      <BsArrowLeft/>
+    </div>
+  );
+}
+
+function App() {
+
+  const [slideIndex, setSlideIndex] = useState(0);
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    beforeChange: (current, next)=>setSlideIndex(next),
+    centerMode: true,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />
+  };
+
+  return (
+    <div className="container">
+      <h2 className='header'>Modern React Carusel</h2>
+        <div className="slider">
+      <Slider {...settings}>
+          {
+            images.map((img, index)=>(
+              <div className={index === slideIndex ? 'slide slide-active': 'slide'} key={index}>
+                <img src={img} alt="" />
+              </div>
+            ))
+          }
+      </Slider>
+        </div>
+    </div>
+  );
+}
+
+export default App;
